48397c28356d0e8641fcac20af36d9ef6e216c01
pupipatsk <pupipat.sk@gmail.com>
2025-05-04 22:28:24 +0700

    base-8A100 ver

---
+++ modal/inference-base.py
@@ -0,0 +1,205 @@
+# -- Imports --
+import os
+from pathlib import Path
+from pydantic import BaseModel
+import torch
+import torch.nn.functional as F
+from transformers import AutoModel, AutoTokenizer
+import numpy as np
+import modal
+
+app = modal.App("inference-llada-base")
+mask_token_id = 126336
+
+weight_volume = modal.Volume.from_name("model-weights-vol", create_if_missing=True)
+MODEL_DIR = Path("/models")
+
+model_id = "GSAI-ML/LLaDA-8B-Instruct"
+model_path = os.path.join(MODEL_DIR, model_id)
+
+image = modal.Image.debian_slim(python_version="3.11").pip_install(
+    "pandas",
+    "torch",
+    "datasets",
+    "tqdm",
+    "transformers",
+    "huggingface_hub",
+    "fastapi[standard]",
+)
+
+
+def select_device():
+    """Selects the best available device (CUDA, MPS, or CPU)."""
+    if torch.cuda.is_available():
+        device = "cuda"
+    elif torch.backends.mps.is_available():
+        device = "mps"
+    else:
+        device = "cpu"
+    print(f"Using device: {device}")
+    return device
+
+
+# Generation Functions
+def add_gumbel_noise(logits, temperature):
+    """Adds Gumbel noise to logits for sampling."""
+    if temperature == 0:
+        return logits
+    logits = logits.to(torch.float64)
+    noise = torch.rand_like(logits, dtype=torch.float64)
+    gumbel_noise = (-torch.log(noise)) ** temperature
+    return logits.exp() / gumbel_noise
+
+
+def get_num_transfer_tokens(mask_index, steps):
+    """Calculates the number of tokens to transfer at each generation step."""
+    mask_num = mask_index.sum(dim=1, keepdim=True)
+    base = mask_num // steps
+    remainder = mask_num % steps
+    num_transfer_tokens = (
+        torch.zeros(
+            mask_num.size(0), steps, device=mask_index.device, dtype=torch.int64
+        )
+        + base
+    )
+    for i in range(mask_num.size(0)):
+        num_transfer_tokens[i, : remainder[i]] += 1
+    return num_transfer_tokens
+
+
+@torch.no_grad()
+def generate(
+    model,
+    prompt,
+    steps=128,
+    gen_length=128,
+    block_length=128,
+    temperature=0.0,
+    cfg_scale=0.0,
+    remasking="low_confidence",
+    mask_id=126336,
+):
+    """Generates text using the trained model."""
+    x = torch.full((1, prompt.shape[1] + gen_length), mask_id, dtype=torch.long).to(
+        model.device
+    )
+    x[:, : prompt.shape[1]] = prompt.clone()
+    prompt_index = x != mask_id
+
+    assert gen_length % block_length == 0
+    num_blocks = gen_length // block_length
+    assert steps % num_blocks == 0
+    steps_per_block = steps // num_blocks
+
+    for num_block in range(num_blocks):
+        start_idx = prompt.shape[1] + num_block * block_length
+        end_idx = start_idx + block_length
+        block_mask_index = x[:, start_idx:end_idx] == mask_id
+        num_transfer_tokens = get_num_transfer_tokens(block_mask_index, steps_per_block)
+
+        for i in range(steps_per_block):
+            mask_index = x == mask_id
+            if cfg_scale > 0:
+                un_x = x.clone()
+                un_x[prompt_index] = mask_id
+                x_ = torch.cat([x, un_x], dim=0)
+                logits = model(x_).logits
+                logits, un_logits = torch.chunk(logits, 2, dim=0)
+                logits = un_logits + (cfg_scale + 1) * (logits - un_logits)
+            else:
+                logits = model(x).logits
+
+            logits_with_noise = add_gumbel_noise(logits, temperature)
+            x0 = torch.argmax(logits_with_noise, dim=-1)
+
+            if remasking == "low_confidence":
+                p = F.softmax(logits.to(torch.float64), dim=-1)
+                x0_p = torch.squeeze(
+                    torch.gather(p, dim=-1, index=x0.unsqueeze(-1)), -1
+                )
+            elif remasking == "random":
+                x0_p = torch.rand_like(x0, device=x0.device)
+            else:
+                raise NotImplementedError(
+                    f"Remasking strategy '{remasking}' not implemented."
+                )
+
+            x0_p[:, prompt.shape[1] + (num_block + 1) * block_length :] = -np.inf
+            x0 = torch.where(mask_index, x0, x)
+            confidence = torch.where(mask_index, x0_p, -np.inf)
+
+            transfer_index = torch.zeros_like(x0, dtype=torch.bool, device=x0.device)
+            for j in range(confidence.shape[0]):
+                _, select_index = torch.topk(confidence[j], k=num_transfer_tokens[j, i])
+                transfer_index[j, select_index] = True
+            x[transfer_index] = x0[transfer_index]
+
+    return x
+
+
+class InferenceRequest(BaseModel):
+    prompt: str
+
+
+class InferenceResponse(BaseModel):
+    summary: str
+
+
+@app.cls(
+    gpu="a100-40gb",
+    volumes={MODEL_DIR: weight_volume},
+    image=image,
+    # scaledown_window=300,
+)
+class Model:
+    @modal.enter()
+    def setup(self):
+        print("Setup tokenizer and model started...")
+        self.device = select_device()
+
+        tokenizer = AutoTokenizer.from_pretrained(
+            os.path.join(MODEL_DIR, "GSAI-ML/LLaDA-8B-Instruct"),
+            trust_remote_code=True,
+            torch_dtype=torch.bfloat16,
+            local_files_only=True,
+        )
+        self.tokenizer = tokenizer
+
+        model = AutoModel.from_pretrained(
+            model_path,
+            trust_remote_code=True,
+            torch_dtype=torch.bfloat16,
+            local_files_only=True,  # Ensure it only loads from local volume
+        )
+        model.to(self.device)
+        model.eval()
+        self.model = model
+        print("Setup tokenizer and model completed.")
+
+    @modal.fastapi_endpoint(method="POST")
+    def inference(self, req: InferenceRequest):
+        print("Received POST request with prompt:", req.prompt)
+        messages = [{"role": "user", "content": req.prompt}]
+        prompt = self.tokenizer.apply_chat_template(
+            messages, add_generation_prompt=True, tokenize=False
+        )
+        input_ids = (
+            torch.tensor(self.tokenizer(prompt)["input_ids"])
+            .to(self.device)
+            .unsqueeze(0)
+        )
+        output_ids = generate(
+            self.model,
+            input_ids,
+            steps=256,
+            gen_length=256,
+            block_length=32,
+            temperature=0.7,
+            cfg_scale=0.0,
+            remasking="low_confidence",
+            mask_id=mask_token_id,
+        )
+        summary = self.tokenizer.batch_decode(
+            output_ids[:, input_ids.shape[1] :], skip_special_tokens=True
+        )[0]
+        return InferenceResponse(summary=summary)
