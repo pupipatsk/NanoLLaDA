e2909b44eb0ce85c08c7b27056d3d94f68f24bd5
กวิน รัตนพันธ์ <43963760+athensclub@users.noreply.github.com>
2025-05-04 15:46:57 +0700

    upload script

---
+++ modal/upload.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+
+import modal
+
+# create a Volume, or retrieve it if it exists
+volume = modal.Volume.from_name("model-weights-vol", create_if_missing=True)
+MODEL_DIR = Path("/models")
+
+# define dependencies for downloading model
+download_image = (
+    modal.Image.debian_slim()
+    .pip_install("huggingface_hub[hf_transfer]")  # install fast Rust download client
+    .env({"HF_HUB_ENABLE_HF_TRANSFER": "1"})  # and enable it
+)
+
+# define dependencies for running model
+inference_image =  modal.Image.debian_slim().pip_install("transformers")
+
+@app.function(
+    volumes={MODEL_DIR: volume},  # "mount" the Volume, sharing it with your function
+    image=download_image,  # only download dependencies needed here
+)
+def download_model(
+    repo_id: str="hf-internal-testing/tiny-random-GPTNeoXForCausalLM",
+    revision: str=None,  # include a revision to prevent surprises!
+    ):
+    from huggingface_hub import snapshot_download
+
+    snapshot_download(repo_id=repo_id, local_dir=MODEL_DIR / repo_id)
+    print(f"Model downloaded to {MODEL_DIR / repo_id}")
